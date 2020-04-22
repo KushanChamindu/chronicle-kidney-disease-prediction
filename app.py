347e043c3dfe740a0fb70a5c2daf019d083c3187
Kushan <ra.kushan1997@gmail.com>
2020-04-22 15:10:39 +0530

    Machine learning model for predict disease

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import numpy as np
+# import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 import pickle
+from flask import make_response
 
 app = Flask(__name__)
 model = pickle.load(open('venv/model.pkl', 'rb'))
@@ -12,18 +13,21 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['get'])
 def predict():
-    int_features = [float(x) for x in request.form.values()]
+    # print(int(request.args['age']))
+    # int_features = [float(x) for x in request.form.values()]
+    int_features=request.args
     final_features = pd.DataFrame(
-        {'age': [int_features[0]/80], 'bp': [int_features[1]/10], 'al':[ int_features[2]/5], 'pcc': [int_features[3]],
-         'bgr': [int_features[4]/146], 'bu': [int_features[5]/418], 'sod': [int_features[6]/164], 'pot': [int_features[7]/130.1],
-         'hemo': [int_features[8]/21.7], 'rbcc': [int_features[9]/12.8], 'dm': [int_features[10]], 'appet': [int_features[11]]})
+        {'age': [float(int_features['age'])/80], 'bp': [float(int_features['bp'])/10], 'al':[ float(int_features['al'])/5], 'pcc': [float(int_features['pcc'])],
+         'bgr': [float(int_features['bgr'])/146], 'bu': [float(int_features['bu'])/418], 'sod': [float(int_features['sod'])/164], 'pot': [float(int_features['pot'])/130.1],
+         'hemo': [float(int_features['hemo'])/21.7], 'rbcc': [float(int_features['rbcc'])/12.8], 'dm': [float(int_features['dm'])], 'appet': [float(int_features['appet'])]})
     prediction = model.predict(final_features)
     print(prediction[0][0])
-    pred=[1 if y>=0.5 else 0 for y in prediction]
+    pred=str(prediction[0][0]*0.96)
 
-    return render_template('index.html', prediction_text='you have this disease ${}'.format(pred))
+    # return render_template('index.html', prediction_text='you have this disease ${}'.format(pred))
+    return make_response(pred)
 
 
 if __name__ == "__main__":
